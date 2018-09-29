8cefc1ffb7157667de25ce834e3f68c3801f501a
TimG <timgm3@gmail.com>
2018-09-29 13:26:41 -0700

    file structure, server.js first setup

---
+++ db/seeds.sql
@@ -0,0 +1,1 @@
+INSERT INTO burgers (burger_name) VALUES ("Double Double"), ("Double Quarter Pounder"), ("Baconator"); 
---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+
+    console.log("connected as id " + connection.threadId);
+});
