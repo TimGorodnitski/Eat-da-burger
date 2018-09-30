49f2777d8f41938e645385f39c49b1dd6509f41d
Tim Gorodnitski <timgm3@gmail.com>
2018-09-30 11:16:05 -0700

    devour buttons working

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,40 @@
 let connection = require("../config/connection.js");
 
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+function objToSql(ob) {
+    var arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+        }
+    }
+
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
+
+
+
 let orm = {
     selectAll: function (table, cb) {
         let queryString = "SELECT * FROM " + table + ";";
@@ -17,7 +52,7 @@ let orm = {
         queryString += cols.toString();
         queryString += ") ";
         queryString += "VALUES (";
-        queryString += vals;
+        queryString += printQuestionMarks(vals.length);
         queryString += ") ";
 
         console.log(queryString);
@@ -30,7 +65,7 @@ let orm = {
     updateOne: function (table, objColVals, condition, cb) {
         let queryString = "UPDATE " + table;
         queryString += " SET ";
-        queryString += objColVals;
+        queryString += objToSql(objColVals);
         queryString += " WHERE ";
         queryString += condition;
 
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,7 +5,7 @@ let burger = require("../models/burger.js");
 
 router.get("/", function (req, res) {
     burger.selectAll(function (data) {
-        let hbsObject = { burger: data };
+        let hbsObject = { burgers: data };
         console.log(hbsObject);
         res.render("index", hbsObject);
     });
